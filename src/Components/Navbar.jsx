9467fa2c0565cb9414956168c1331cdf1524a830
gazi-marvelminds <gazi.marvelminds@gmail.com>
2024-02-26 18:52:44 +0530

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,9 +12,9 @@ const Navbar = () => {
 		<div className='w-full px-6 fixed top-0 left-0 z-50 backdrop-blur-sm py-2 lg:px-44  flex flex-col md:flex-row justify-between  '>
 			<Nav collapse={collapse} />
 			<div className=' w-full md:w-1/2 flex justify-between '>
-				<h1 className=' p-1 whitespace-nowrap text-sm md:text-2xl dark:text-zinc-200 text-blue-500 font-semibold '>
+				<h1 className='whitespace-nowrap text-sm md:text-2xl dark:text-zinc-200 text-blue-500 font-semibold '>
 					Gazi
-					<span className=' font-bold dark:text-white text-gray-900'> Wani</span>
+					<span className=' font-bold dark:text-white text-gray-900'>Wani</span>
 				</h1>
 				<ThemeBtn />
 				<button
--- src/Components/WhatIdo.jsx
+++ src/Components/WhatIdo.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const WhatIdo = () => {
 	return (
-		<div className=' my-20 lg:px-44'>
-			<div className=' py-3 rounded-md px-4 gap-3  dark:bg-gray-800 bg-zinc-50 shadow-md shadow-gray-100 dark:shadow-slate-800 border  flex justify-between flex-col'>
+		<div className=' my-20 px-5 lg:px-44'>
+			<div className=' py-3 rounded-md px-4 gap-3  dark:bg-gray-800 dark:shadow-slate-800 border  flex justify-between flex-col'>
 				<h1 className=' text-xl dark:text-white text-slate-900'>Work Experience</h1>
 				<div className='flex justify-between'>
 					<h2 className='text-xl dark:text-white text-slate-900'>Red Stag Labs</h2>
